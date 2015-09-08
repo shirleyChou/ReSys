adfffc1a59ced10cd2c2dff427dc33e41f75ab61
zhouyi <zhouyi@ibuka.cn>
2015-09-08 14:07:14 +0800

    create new class

---
+++ fp_tree.py
@@ -0,0 +1,18 @@
+# encoding utf-8
+
+
+class FPTree:
+    def __init__(self, name, num_of_occur, node_of_parent):
+        self.node_name = name
+        self.count = num_of_occur
+        self.link_of_node = None
+        self.parent = node_of_parent
+        self.child = {}
+
+    def increase(self, num_of_occur):
+        self.count += num_of_occur
+
+    def disp(self, index=1):
+        print '' * index, self.node_name, '', self.count
+        for child in self.child.values():
+            child.disp(index + 1)
